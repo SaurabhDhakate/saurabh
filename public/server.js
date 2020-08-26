ac74bd219722ddc6b63543020e1475198522bdc8
SaurabhDhakate <saurabhdhakate25@gmail.com>
2020-08-26 13:56:32 +0530

    api

--- public/server.js
+++ public/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080
@@ -7,5 +8,8 @@ app.get('/year',(req,res)=>{
     var ids = req.query
     res.json({"Season" : ids.year})
 } )
+
+console.log(port)
+
 
 app.listen(port)
